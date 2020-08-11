ece4f899ccdf4bb6fc0786953b0da4300410a0ca
Fernando Batista <nandosbx@gmail.com>
2020-08-11 00:46:06 -0300

    Adding Eslint and Prettier

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.use('/api', require('./routes/index'))
+app.use("/api", require("./routes/index"));
 
 const config = require("./config/key");
 
-const mongoose = require('mongoose');
-mongoose.connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+const mongoose = require("mongoose");
+mongoose.connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log('MongoDB database connection established successfully.'))
-  .catch(err => console.log(err));
+  .then(() =>
+    console.log("MongoDB database connection established successfully.")
+  )
+  .catch((err) => console.log(err));
 
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on Port: ${PORT}` )
-})
+  console.log(`Server is running on Port: ${PORT}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,147 +1,189 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      id: 0,
-      name:"",
-      email:"",
-      password:""
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [],
+            id: 0,
+            name: '',
+            email: '',
+            password: '',
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/api')
-      .then((res) => {
-        this.setState({
-          users: res.data,
-          id:0,
-          name:'',
-          email:'',
-          password:''
+    componentDidMount() {
+        axios.get('http://localhost:5000/api').then((res) => {
+            this.setState({
+                users: res.data,
+                id: 0,
+                name: '',
+                email: '',
+                password: '',
+            })
+            //console.log(data)
         })
-        console.log(data)
-      })
-  }
-
-  nameChange = event => {
-    this.setState({
-      name:event.target.value
-    })
-  }
-
-  emailChange = event => {
-    this.setState({
-      email:event.target.value
-    })
-  }
-
-  passwordChange = event => {
-    this.setState({
-      password:event.target.value
-    })
-  }
-
-  submit(event, id) {
-    event.preventDefault()
-    if(id === 0) {
-    axios.post('http://localhost:5000/api', {name:this.state.name, email:this.state.email, password: this.state.password})
-    } else {
-      axios.put(`http://localhost:5000/${id}`,{name:this.state.name, email:this.state.email, password: this.state.password})
     }
-  }
 
+    nameChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        })
+    }
 
+    emailChange = (event) => {
+        this.setState({
+            email: event.target.value,
+        })
+    }
 
-  render() {
-    return (
-      <div className="row">
-        <div className="col s6"></div>
+    passwordChange = (event) => {
+        this.setState({
+            password: event.target.value,
+        })
+    }
 
-        <form onSubmit={e => this.submit(e, this.state.id)}>
-          <div class="input-field col s12">
-            <i class="material-icons prefix">person</i>
-            <input onChange={e =>this.nameChange(e)} type="text" id="autocomplete-input" class="autocomplete" />
-            <label for="autocomplete-input">Name</label>
-          </div>
+    submit(event, id) {
+        event.preventDefault()
+        if (id === 0) {
+            axios.post('http://localhost:5000/api', {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+            })
+        } else {
+            axios.put(`http://localhost:5000/api/${id}`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+            })
+            this.componentDidMount()
+        }
+    }
 
-          <div class="input-field col s12">
-            <i class="material-icons prefix">email</i>
-            <input onChange={e =>this.emailChange(e)}type="email" id="autocomplete-input" class="autocomplete" />
-            <label for="autocomplete-input">Email</label>
-          </div>
+    edit(id) {
+        axios.get(`http://localhost:5000/api/${id}`).then((res) => {
+            this.setState({
+                id: res.data._id,
+                name: res.data.name,
+                email: res.data.email,
+                password: res.data.password,
+            })
+        })
+    }
 
-          <div class="input-field col s12">
-            <i class="material-icons prefix">vpn_key</i>
-            <input onChange={e =>this.passwordChange(e)}
-              type="password"
-              id="autocomplete-input"
-              class="autocomplete"
-            />
-            <label for="autocomplete-input">Password</label>
-          </div>
+    delete(id) {
+        axios.delete(`http://localhost:5000/api/${id}`).then(() => {
+            this.componentDidMount()
+        })
+    }
 
-          <button
-            class="btn waves-effect waves-light right"
-            type="submit"
-            name="action"
-          >
-            Submit
-            <i class="material-icons right">send</i>
-          </button>
-        </form>
-        <div className="col s6">
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Edit</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
+    render() {
+        return (
+            <div className="row">
+                <div className="col s6"></div>
 
-            <tbody>
-              {this.state.users.map(user => 
-                <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.password}</td>
+                <form onSubmit={(e) => this.submit(e, this.state.id)}>
+                    <div className="input-field col s12">
+                        <i className="material-icons prefix">person</i>
+                        <input
+                            value={this.state.name}
+                            onChange={(e) => this.nameChange(e)}
+                            type="text"
+                            id="autocomplete-input"
+                            className="autocomplete"
+                        />
+                        <label htmlFor="autocomplete-input">Name</label>
+                    </div>
 
-                <td>
-                  <button
-                    onClick={e=>this.edit(e._id)}
-                    class="btn waves-effect waves-light"
-                    type="submit"
-                    name="action"
-                  >
-                    <i class="material-icons">edit</i>
-                  </button>
-                </td>
+                    <div className="input-field col s12">
+                        <i className="material-icons prefix">email</i>
+                        <input
+                            value={this.state.email}
+                            onChange={(e) => this.emailChange(e)}
+                            type="email"
+                            id="autocomplete-input"
+                            className="autocomplete"
+                        />
+                        <label htmlFor="autocomplete-input">Email</label>
+                    </div>
 
-                <td>
-                  <button
-                    onClick={e=>this.delete(e._id)}
-                    class="btn waves-effect waves-light"
-                    type="submit"
-                    name="action"
-                  >
-                    <i class="material-icons">delete</i>
-                  </button>
-                </td>
-                </tr>                
-              )}              
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
+                    <div className="input-field col s12">
+                        <i className="material-icons prefix">vpn_key</i>
+                        <input
+                            value={this.state.password}
+                            onChange={(e) => this.passwordChange(e)}
+                            type="password"
+                            id="autocomplete-input"
+                            className="autocomplete"
+                        />
+                        <label htmlFor="autocomplete-input">Password</label>
+                    </div>
+
+                    <button
+                        className="btn waves-effect waves-light right"
+                        type="submit"
+                        name="action"
+                    >
+                        Submit
+                        <i className="material-icons right">send</i>
+                    </button>
+                </form>
+                <div className="col s6">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Password</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {this.state.users.map((user) => (
+                                <tr key={user._id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.password}</td>
+
+                                    <td>
+                                        <button
+                                            onClick={(e) => this.edit(user._id)}
+                                            className="btn waves-effect waves-light"
+                                            type="submit"
+                                            name="action"
+                                        >
+                                            <i className="material-icons">
+                                                edit
+                                            </i>
+                                        </button>
+                                    </td>
+
+                                    <td>
+                                        <button
+                                            onClick={(e) =>
+                                                this.delete(user._id)
+                                            }
+                                            className="btn waves-effect waves-light"
+                                            type="submit"
+                                            name="action"
+                                        >
+                                            <i className="material-icons">
+                                                delete
+                                            </i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
